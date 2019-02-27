c02efacb751e97eb1a9cfcfd8bf2e0ef7644ced8
Nigel Wade <nigeljohnwade@gmail.com>
2019-02-27 13:52:54 +0000

    Creates a films context HOC

--- src/Films.js
+++ src/Films.js
@@ -1,34 +1,28 @@
 import React, { Fragment } from 'react';
 
-import { FilmsContext } from './FilmsContext';
+import { withFilms } from './FilmsContext';
 import Heading from './components/Heading';
 import UnorderedList from './components/UnorderedList';
 
-const Films = () => {
+const Films = (props) => {
     return (
-        <FilmsContext.Consumer>
-            {(films) => {
-                return (
-                    <Fragment>
-                        <Heading
-                            headingText="Films"
-                            headingSubText={films.count ? `Data about all ${films.count} star wars films` : null}
-                        />
-                        {
-                            films.results &&
-                            <UnorderedList items={films.results.map((item) => {
-                                return {
-                                    key: item.url,
-                                    listItem: item.title,
-                                }
-                            })} />
-                        }
-                    </Fragment>
-                )
-            }}
-        </FilmsContext.Consumer>
+        <Fragment>
+            <Heading
+                headingText="Films"
+                headingSubText={props.films.count ? `Data about all ${props.films.count} star wars films` : null}
+            />
+            {
+                props.films.results &&
+                <UnorderedList items={props.films.results.map((item) => {
+                    return {
+                        key: item.url,
+                        listItem: item.title,
+                    }
+                })} />
+            }
+        </Fragment>
     )
 
 }
 
-export default Films;
+export default withFilms(Films);
