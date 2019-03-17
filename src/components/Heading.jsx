00e0e2de1f0f886f57dda1376e7906f75b850b5d
nigeljohnwade <nigeljohnwade@gmail.com>
2019-03-17 17:55:07 +0000

    Adjust heading level

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -14,7 +14,7 @@ const Heading = ({
                 src &&
                 <img src={src} alt="*" />
             }
-            <h1>{headingText}</h1>
+            <h2>{headingText}</h2>
             {
                 headingSubText &&
                 <p className="sub-heading">{headingSubText}</p>
